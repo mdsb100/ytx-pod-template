3e39ed099480571289f69f1e9c9d5e2905cc7028
caojun <78612846@qq.com>
2016-08-17 15:44:56 +0800

    fix pod_package.sh

--- pod_package.sh
+++ pod_package.sh
@@ -5,12 +5,12 @@ PROJECT_NAME=${PWD##*/}
 IS_SOURCE=1 pod package ${PROJECT_NAME}.podspec --exclude-deps --no-mangle --library --spec-sources=http://gitlab.baidao.com/ios/ytx-pod-specs.git,https://github.com/CocoaPods/Specs.git
 
 BINARY_DIR=$(ls -l | grep ^d | grep -o "${PROJECT_NAME}-.*")
-cp -rp $BINARY_DIR/ios/ Pod/lib
+cp -rp $BINARY_DIR/ios/ $PROJECT_NAME/lib
 
 rm -rf $BINARY_DIR
 
 # 去除.a armv7s i386的架构
-cd Pod/lib
+cd $PROJECT_NAME/lib
 
 lipo "lib${PROJECT_NAME}.a" -remove armv7s -remove i386 -output "lib${PROJECT_NAME}.a"
 # lipo "lib${PROJECT_NAME}.a" -thin armv7 -output "lib${PROJECT_NAME}-armv7.a"
