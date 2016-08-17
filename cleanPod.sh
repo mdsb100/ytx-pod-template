8378fd05cb813964e21b239996bf5ba29816b02a
caojun <78612846@qq.com>
2016-08-17 15:08:11 +0800

    增加cleanPod.sh

---
+++ cleanPod.sh
@@ -0,0 +1,27 @@
+PROJECT_NAME=${PWD##*/}
+
+POD_NAME=$1
+
+if [[ ! $POD_NAME ]]; then
+	echo "pod cache clean --all"
+	cd YtxMobile
+	rm -rf Pods
+	pod cache clean --all
+	echo "pod install"
+	pod install
+else
+	cd YtxMobile/Pods
+	DIRNAME=$(ls -l | grep ^d | grep -o ${POD_NAME})
+	if [[ ! $DIRNAME ]]; then
+		echo "Pod name is error, Please re operate！"
+ 		exit 0
+	else
+		echo "pod cache clean -$POD_NAME"
+		rm -rf '$POD_NAME'
+ 	    rm -rf Pods.xcodeproj 
+ 	    cd ..
+ 	    pod cache clean -$POD_NAME
+ 	    echo "pod install"
+ 	    pod install	
+	fi
+fi
