185888e8fb9aaac54e985f734d80e3fc3e60c9b7
caojun <78612846@qq.com>
2016-09-01 15:19:32 +0800

    删除pod cache clean —all

--- lint.sh
+++ lint.sh
@@ -1,4 +1,3 @@
 SOURCES='http://gitlab.baidao.com/ios/ytx-pod-specs.git,master'
 IS_SOURCE=1 pod lib lint --sources=$SOURCES --verbose --fail-fast --use-libraries
-pod cache clean --all
 pod lib lint --sources=$SOURCES --verbose --use-libraries --fail-fast --use-libraries
